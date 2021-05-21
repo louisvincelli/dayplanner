add8eabebf0b0cdb59e5c27d8b84219070a8ef69
Louie Vincelli <louisvincelli@gmail.com>
2021-05-20 23:07:12 -0700

    final

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+var currentDay = document.querySelector("#currentDay");
+currentDay.innerText = moment().format('LL');
+
 var calendar = $("#calendar");
 for (let i = 9; i< 18; ++i){
     var div = $("<div>").addClass("calendar-box");
@@ -5,25 +8,49 @@ for (let i = 9; i< 18; ++i){
     var time = `${i}:00:00`;
     var formattedTime = moment(time,"HH:mm:ss").format("hh:mm A")
     var timediv = $("<div>").addClass("time-div").text(formattedTime);
+    var button = $("<button>").addClass("save-btn").text("Save");
     var lockdiv = $("<div>").addClass("lock");
-    var img = $("<img>").attr("src","path to image here");
-    lockdiv.append(img);
+    //var img = $("<img>").attr("src","lockimg2.jpeg");
+    //lockdiv.append(img);
+    lockdiv.append(button);
     div.append(timediv);
     div.append(input);
     div.append(lockdiv);
     calendar.append(div);
+
+    // //if(current time < 9am && >7am){
+    //     query select text area .style("background-color:gray")
+    //      momment <8 && >7.
+    //  else if(moment<9am && )
+    //   if current time <9am all blocks 
+    // }
+    
 }
 
+var userInput = localStorage.getItem('userInput');
 
-// var isWeekend = day => {
-//     //6 when its saturday, 0 - sunday
-//     return day %7 ===6 || day % 7 === 0;
-// }
+if (userInput) {
+    input.textContent = userInput
+}
 
-// var getDayName = day => {
-//     var date = new Date(Date.UTC(2018,0,day));
+// // userInput.addEventListener('input', letter => {
+// //     text.textContent = letter.target.value
+// // })
 
-//     return new Intl.DateTimeFormat("en-US", { weekday: "short" }) format(date);
+function save() {
+    localStorage.setItem('userInput', input.textContent);
+};
 
-// }
+for (i = 0; i < button.length; i++) {
+    button[i].addEventListener("click", save);
+}
+// function save(){
+//     var userInput = input.value;
 
+//     if (userInput) {
+//         localStorage.setItem('workinput', input.textContent);
+//         input.innertext += `${userInput}`;
+//     }
+// };
+
+//button.addEventListener("click", save);
